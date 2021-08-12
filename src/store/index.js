75cc2f1ff0610a83df7f78a361fd703a011f5a99
svetanguyen <matematicaforeber@gmail.com>
2021-08-13 00:15:53 +0700

    add data to projects

--- src/store/index.js
+++ src/store/index.js
@@ -6,10 +6,10 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
       projectCards: [
-        { projectTitle: "Oldflamelabel", projectCoverPhoto: "stock-1" },
-        { projectTitle: "Talor", projectCoverPhoto: "stock-2" },
-        { projectTitle: "Pokemon desktop game", projectCoverPhoto: "stock-3" },
-        { projectTitle: "Instagram clone", projectCoverPhoto: "stock-4" },
+        { projectTitle: "Oldflamelabel", projectCoverPhoto: "Old flame", technologies: "Shopify, Liquid, JS, HTML/CSS", link: "https://oldflamelabel.com/" },
+        { projectTitle: "Talor", projectCoverPhoto: "Talor", technologies: "Shopify, Liquid, JS, HTML/CSS", link: "https://www.talor.co.uk/" },
+        { projectTitle: "Pokemon desktop game", projectCoverPhoto: "pokemon", technologies: "Vanilla JS, HTML/CSS" },
+        { projectTitle: "Instagram clone", projectCoverPhoto: "IG clone", technologies: "React, Tailwind, HTML/CSS" },
       ]
     },
     mutations: {      
