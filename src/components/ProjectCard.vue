bab21e7d6c9234bb21998dd2004290bd69288edd
svetanguyen <258005_evn@etgrp.onmicrosoft.com>
2021-11-04 17:07:05 +0700

    set height for image wrappers

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="project-card">
     <a :href="work.link" class="project-wrapper bordered-container">
-       <div class="top-text">{{ work.projectTitle }}</div>
+    <div>
+      <div class="top-text">{{ work.projectTitle }}</div>
         <div class="img-wrapper">
           <img class="scale" :src="require(`../assets/images/${work.projectCoverPhoto}.jpg`)" :alt="work.projectTitle">
           <img :src="require(`../assets/images/${work.projectCoverPhoto}.jpg`)" :alt="work.projectTitle">
         </div>
         <div class="description">{{ work.technologies }}</div>
+    </div> 
     </a>
      
   </div>
@@ -27,10 +29,18 @@ export default {
   }
   .project-wrapper {
     display: block;
-    height: 100%;
+    height: 0;
+    padding: 100% 0 0;
     position: relative;
-    padding: 46px 52px 1px;
     background: linear-gradient(0deg, rgba(255, 255, 255, 0.24) 0%, rgba(249, 249, 249, 0.2175) 0.01%, rgba(234, 234, 234, 0.152945) 13.54%, rgba(229, 229, 229, 0.133462) 13.55%, rgba(222, 222, 222, 0.107122) 29.68%, rgba(216, 216, 216, 0.0799167) 29.69%, rgba(211, 211, 211, 0.0596295) 45.82%, rgba(205, 205, 205, 0.0361583) 45.83%, rgba(202, 202, 202, 0.0242387) 63.53%, rgba(199, 199, 199, 0.0134659) 63.54%, rgba(197, 197, 197, 0.00299334) 79.17%, rgba(198, 198, 198, 0.00684193) 79.18%, rgba(196, 196, 196, 0) 100%);
+    & > div {
+      position: absolute;
+      top: 0;
+      left: 0;
+      padding: 46px 52px 1px;
+      width: 100%;
+      height: 100%;
+    }
   }
   img {
     max-width: 100%;
