7b6e2659f43787366eacf6680f05ddc89aff9161
svetanguyen <matematicaforeber@gmail.com>
2021-12-03 10:23:15 +0700

    change title

--- src/router/index.js
+++ src/router/index.js
@@ -49,7 +49,7 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  document.title = `${to.meta.title} | Fireblog`;
+  document.title = `${to.meta.title} | Portfolio`;
   next();
 })
 
