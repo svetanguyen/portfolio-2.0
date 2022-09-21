d124ea84fa28773165ea3c702391d0a56973c742
svetanguyen <matematicaforeber@gmail.com>
2022-09-21 11:54:59 -0400

    update projects

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,6 @@ Vue.use(Vuex)
 export default new Vuex.Store({
     state: {
       projectCards: [
-        { projectTitle: "Oldflamelabel", projectCoverPhoto: "Old flame", technologies: "Shopify, Liquid, JS, HTML/CSS", link: "https://oldflamelabel.com/" },
-        { projectTitle: "Talor", projectCoverPhoto: "Talor", technologies: "Shopify, Liquid, JS, HTML/CSS", link: "https://www.talor.co.uk/" },
         { projectTitle: "Pokemon desktop game", projectCoverPhoto: "pokemon", technologies: "Vanilla JS, HTML/CSS", link: "https://pokemon-fight-game.netlify.app/" },
         { projectTitle: "Instagram clone", projectCoverPhoto: "IG clone", technologies: "React, Tailwind, HTML/CSS, Firebase", link: "https://insta-clone-5b854c.netlify.app/" },
       ]
